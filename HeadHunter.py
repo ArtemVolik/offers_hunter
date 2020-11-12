5f0e98412bdbb5191e4edbcd3501f91ac2cf356a
artyom <art.volyk@gmail.com>
2020-11-13 00:52:38 +0200

    add dict update

--- HeadHunter.py
+++ HeadHunter.py
@@ -14,16 +14,16 @@ def get_response(url, params=None):
     return response.json()
 
 
-def get_specialization(url):
+def get_specialization(url, specialization):
     specializations = get_response(url)
-    programming_vacancy = [(item['name'], item['id']) for line in specializations for item in line['specializations'] \
-                           if 'программирование' in item['name'].lower()]
+    programming_vacancy = [(item['name'], item['id']) for line in specializations for item in line['specializations']
+                           if specialization in item['name'].lower()]
     return programming_vacancy[0][1]
 
 
 def get_programist_vacansies(url, language, page=''):
     params = dict()
-    params['specialization'] = get_specialization(urls['specialization'])
+    params['specialization'] = get_specialization(urls['specialization'], 'программирование')
     params['period'] = 30
     params['area'] = 1
     params['text'] = language
@@ -42,41 +42,42 @@ def predict_salary(salary_from, salary_to):
         return int(salary_to * 0.8)
 
 
-response = requests.get('https://habr.com/ru/post/310262/')
-response.raise_for_status()
-soup = BeautifulSoup(response.content, features='lxml')
-source_headers_raw = [header.text.replace(':', '').split() for header in soup.find_all('h3')[:15]]
-languages = {header[3] for header in source_headers_raw if "CSS" not in header if "Shell" not in header}
-program_languages = {language: dict() for language in languages}
+def get_languages(url):
+    response = requests.get(url)
+    response.raise_for_status()
+    soup = BeautifulSoup(response.content, features='lxml')
+    source_headers_raw = [header.text.replace(':', '').split() for header in soup.find_all('h3')[:15]]
+    languages = {header[3] for header in source_headers_raw if "CSS" not in header if "Shell" not in header}
+    return {language: dict() for language in languages}
 
 
-for language in program_languages.keys():
-    vacancies_all_pages = []
-    pages_quantity = get_programist_vacansies(urls['vacancies'], language)['pages']
-    vacancies_all_pages = [get_programist_vacansies(urls['vacancies'], language, page) for page in range(pages_quantity)]
-    vacancies_found = 0
-    salaries_sum = 0
-    processed_vacancy_count = 0
-    for page in vacancies_all_pages:
-        vacancies_found += page['found']
-        page_predicted_salaries = [predict_salary(vacansy['salary']['from'], vacansy['salary']['to']) if vacansy['salary']['currency'] == 'RUR' \
+if __name__ == '__main__':
+
+    program_languages = get_languages('https://habr.com/ru/post/310262/')
+
+    for language in program_languages.keys():
+
+        pages_quantity = get_programist_vacansies(urls['vacancies'], language)['pages']
+        vacancies_all_pages = [get_programist_vacansies(urls['vacancies'], language, str(page)) for page in
+                               range(pages_quantity)]
+        vacancies_found: int = 0
+        salaries_sum: int = 0
+        processed_vacancy_count: int = 0
+
+        for page in vacancies_all_pages:
+            vacancies_found += page['found']
+            page_predicted_salaries = [
+                predict_salary(vacansy['salary']['from'], vacansy['salary']['to']) if vacansy['salary'][
+                                                                                          'currency'] == 'RUR'
                 else None for vacansy in page['items'] if vacansy['salary'] is not None]
-        page_salaries = [salary for salary in page_predicted_salaries if salary is not None]
-        salaries_sum += sum(page_salaries)
-        processed_vacancy_count += len(page_salaries)
-    program_languages[language]['vacancies_found'] = vacancies_found
-    program_languages[language]['avarage_salary'] = int(salaries_sum / processed_vacancy_count)
-    program_languages[language]["vacancies_processed"] = processed_vacancy_count
+            page_salaries = [salary for salary in page_predicted_salaries if salary is not None]
+            salaries_sum += sum(page_salaries)
+            processed_vacancy_count += len(page_salaries)
 
-pprint(program_languages)
+        program_languages[language].update({
+            'vacancies_found': vacancies_found,
+            'avarage_salary': int(salaries_sum / processed_vacancy_count),
+            'vacancies_processed': processed_vacancy_count
+        })
 
-
-
-
-
-# vacancies_all_pages = [get_programist_vacansies(urls['vacancies'], 'JavaScript', 1)]
-# vacancies = vacancies_all_pages[0]['items']
-# pprint(vacancies)
-# pprint([vacancy['salary'] for vacancy in vacancies])
-
-# print(salaries)
+    pprint(program_languages)
