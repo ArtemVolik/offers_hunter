a3e8e1df0a50db3049ce3e406118eb1bf5bcd528
artyom <art.volyk@gmail.com>
2020-11-13 02:41:13 +0200

    replace common used functions to func.py add table to HeadHunter

--- HeadHunter.py
+++ HeadHunter.py
@@ -1,17 +1,9 @@
-import requests
 from pprint import pprint
-from bs4 import BeautifulSoup
-
+from func import get_response, get_languages, predict_salary, print_table_from_dict
 urls = {
     'specialization': 'https://api.hh.ru/specializations',
     'vacancies': 'https://api.hh.ru/vacancies'
 }
-
-
-def get_response(url, params=None):
-    response = requests.get(url, params)
-    response.raise_for_status()
-    return response.json()
 
 
 def get_specialization(url, specialization):
@@ -33,28 +25,9 @@ def get_programist_vacansies(url, language, page=''):
     return get_response(url, params=params)
 
 
-def predict_salary(salary_from, salary_to):
-    if salary_from and salary_to:
-        return int((salary_from + salary_to) / 2)
-    if salary_from:
-        return int(salary_from * 1.2)
-    if salary_to:
-        return int(salary_to * 0.8)
-
-
-def get_languages(url):
-    response = requests.get(url)
-    response.raise_for_status()
-    soup = BeautifulSoup(response.content, features='lxml')
-    source_headers_raw = [header.text.replace(':', '').split() for header in soup.find_all('h3')[:15]]
-    languages = {header[3] for header in source_headers_raw if "CSS" not in header if "Shell" not in header}
-    return {language: dict() for language in languages}
-
-
-if __name__ == '__main__':
+def main():
 
     program_languages = get_languages('https://habr.com/ru/post/310262/')
-
     for language in program_languages.keys():
 
         pages_quantity = get_programist_vacansies(urls['vacancies'], language)['pages']
@@ -76,8 +49,15 @@ if __name__ == '__main__':
 
         program_languages[language].update({
             'vacancies_found': vacancies_found,
-            'avarage_salary': int(salaries_sum / processed_vacancy_count),
-            'vacancies_processed': processed_vacancy_count
+            'vacancies_processed': processed_vacancy_count,
+            'avarage_salary': int(salaries_sum / processed_vacancy_count)
         })
+    return program_languages
 
-    pprint(program_languages)
+
+if __name__ == '__main__':
+    program_languages_statistic = main()
+    print_table_from_dict('HeadHunter Moscow', program_languages_statistic)
+
+
+
---
+++ func.py
@@ -0,0 +1,41 @@
+import requests
+from bs4 import BeautifulSoup
+from terminaltables import AsciiTable
+
+def get_response(url, params=None):
+    response = requests.get(url, params)
+    response.raise_for_status()
+    return response.json()
+
+
+def get_languages(url):
+    response = requests.get(url)
+    response.raise_for_status()
+    soup = BeautifulSoup(response.content, features='lxml')
+    source_headers_raw = [header.text.replace(':', '').split() for header in soup.find_all('h3')[:15]]
+    languages = {header[3] for header in source_headers_raw if "CSS" not in header if "Shell" not in header}
+    return {language: dict() for language in languages}
+
+
+def predict_salary(salary_from, salary_to):
+    if salary_from and salary_to:
+        return int((salary_from + salary_to) / 2)
+    if salary_from:
+        return int(salary_from * 1.2)
+    if salary_to:
+        return int(salary_to * 0.8)
+
+
+def print_table_from_dict(title, input_dict):
+    table_data = [('Язык программирования', 'Вакансий найдено', 'Вакансий обработано', 'Средняя зарплата')]
+    dict_unpacked = [(language[0], *language[1].values()) for language in input_dict.items()]
+    for language_data in dict_unpacked:
+        table_data.append(language_data)
+    table_instance = AsciiTable(table_data, title)
+    table_instance.justify_columns[2] = 'right'
+    print(table_instance.table)
+    print()
+
+
+
+
